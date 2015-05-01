d0c8f9edb0e9eb2d973f94aab8625ef92525232c
Kirill <neikila@gmail.com>
2015-05-01 15:43:47 +0300

    gitignore and sh scripts were corrected

--- assembly.sh
+++ assembly.sh
@@ -1,1 +1,13 @@
+rm target/* -r
 mvn compile assembly:single
+cp target/DBApi-1.0-jar-with-dependencies.jar .
+mkdir server -p
+rm server/* -rf
+cp DBApi-1.0-jar-with-dependencies.jar server/.
+cp -r server_tml server/.
+cp -r public_html server/.
+cp -r cfg server/.
+cp -r data server/.
+cp -r start.sh server/.
+tar czf server.tar.gz server
+mv server.tar.gz server/.
